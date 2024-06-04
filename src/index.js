1d06ef13e7384da39787a10965319d7c6fe90c2d
Steven Pelletier <steve.d.pelletier@gmail.com>
2024-06-03 21:36:31 -0400

    Added dist folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello There!!!')
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
+  console.log("Hello There!")
