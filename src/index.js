b9183981c0b0953857386a090972b167daacbd8b
Steven Pelletier <steve.d.pelletier@gmail.com>
2024-06-03 21:40:30 -0400

    Added Lodash

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import _ from "lodash";
 
-  console.log("Hello There!")
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+console.log("Hello There!");
